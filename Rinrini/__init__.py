2ae124d9f8f55ef3473e6c6dbad210399e3416eb
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2023-07-06 17:06:19 +0530

    Update __init__.py

--- Rinrini/__init__.py
+++ Rinrini/__init__.py
@@ -24,7 +24,7 @@ from pyrogram import Client
 from pyromod import listen
 
 from config import config
-from Rinrini.RinriniGban import RinriniClient
+from RinriniGban import RinriniClient
 
 from Rinrinigban import RinriniClient
 
