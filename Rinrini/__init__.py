63e4eca9fe315acd6a2a390d0aa3142a823dfd85
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2023-07-06 15:45:44 +0530

    Add files via upload

---
+++ Rinrini/__init__.py
@@ -0,0 +1,69 @@
+#    Rinrini (Development)
+#    Copyright (C) 2021 - 2023 Famhawite Infosys (@FamhawiteInfosys)
+#    Copyright (C) 2021 - 2023 Nicky Lalrochhara (@Nickylrca)
+
+#    This program is free software; you can redistribute it and/or modify 
+#    it under the terms of the GNU General Public License as published by 
+#    the Free Software Foundation; either version 3 of the License, or 
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import sys
+import dns.resolver
+
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from pymongo import MongoClient
+from pyrogram import Client
+from pyromod import listen
+
+from config import config
+from Rinrini.RinriniGban import RinriniClient
+
+#from Rinrinigban import RinriniClient
+
+OWNER_ID = config.settings.owner
+BOT_ID = config.telegram.bot.id
+BOT_NAME = config.telegram.bot.name
+BOT_USERNAME = config.telegram.bot.username
+LOG_CHANNEL = config.settings.log.chat_id
+SUDO_USERS = config.settings.sudo_users
+PREFIX = config.settings.commands.prefix
+BACKUP_CHAT = config.settings.backup.chat_id
+
+RinriniCli = Client(
+    session_name='RinriniSession',
+    api_id=config.telegram.api_id,
+    api_hash=config.telegram.api_hash,
+    bot_token=config.telegram.bot.token
+)
+
+# MongoDatabase dns configurations
+dns.resolver.default_resolver=dns.resolver.Resolver(configure=False)
+dns.resolver.default_resolver.nameservers=['8.8.8.8'] 
+
+RinriniAPI = RinriniClient(api_key=config.api.Rinrini.api_key)
+
+# Async scheduler
+scheduler = AsyncIOScheduler()
+
+try:
+  RinriniMongoClient = MongoClient(config.database.database_url)
+  RinriniDB = RinriniMongoClient.Rinrini_mongo
+except:
+  sys.exit(f"{BOT_NAME}'s database hi a nung lo tlat!")
+
+TELEGRAM_SERVICES_IDs = (
+    [
+        777000, # Telegram Service Notifications
+        1087968824 # GroupAnonymousBot
+    ]
+)
+
+GROUP_ANONYMOUS_BOT = 1087968824
---
+++ Rinrini/__main__.py
@@ -0,0 +1,70 @@
+#    Rinrini (Development)
+#    Copyright (C) 2021 - 2023 Famhawite Infosys (@FamhawiteInfosys)
+#    Copyright (C) 2021 - 2023 Nicky Lalrochhara (@Nickylrca)
+
+#    This program is free software; you can redistribute it and/or modify 
+#    it under the terms of the GNU General Public License as published by 
+#    the Free Software Foundation; either version 3 of the License, or 
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+import importlib
+from os.path import dirname
+from sys import platform
+
+from Rinrini import RinriniCli, scheduler
+from Rinrini.plugins import ALL_MODULES
+
+IMPORTED = {}
+HELPABLE = {}
+SUB_MODE = {}
+HIDDEN_MOD = {}
+
+STATS = []
+USER_INFO = []
+
+if platform == "linux" or platform == "linux2":
+    path_dirSec = '/'
+elif platform == "win32":
+    path_dirSec = '\\'
+
+cdir = dirname(__file__) 
+for mode in ALL_MODULES: 
+    module = mode.replace(cdir, '').replace(path_dirSec, '.')
+    imported_module = importlib.import_module('Rinrini' + module)
+    if not hasattr(imported_module, "__mod_name__"):
+        imported_module.__mod_name__ = imported_module.__name__
+ 
+    if not imported_module.__mod_name__.lower() in IMPORTED:
+        IMPORTED[imported_module.__mod_name__.lower()] = imported_module
+    else:
+        raise Exception("Can't have two modules with the same name! Please change one")
+
+    if hasattr(imported_module, "__help__") and imported_module.__help__:
+        HELPABLE[imported_module.__mod_name__.lower()] = imported_module
+    
+    if hasattr(imported_module, "__sub_mod__") and imported_module.__sub_mod__:
+        SUB_MODE[imported_module.__mod_name__.lower()] = imported_module
+    
+    if hasattr(imported_module, "__hidden__") and imported_module.__hidden__:
+        HIDDEN_MOD[imported_module.__mod_name__.lower()] = imported_module.__hidden__
+
+    if hasattr(imported_module, "__stats__"):
+        STATS.append(imported_module)
+
+    if hasattr(imported_module, "__user_info__"):
+        USER_INFO.append(imported_module)
+
+print(HIDDEN_MOD)
+
+if __name__ == "__main__":
+    scheduler.start()
+    RinriniCli.run()
